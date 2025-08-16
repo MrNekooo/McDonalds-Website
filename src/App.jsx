9f90922fea8132ef8d4960582527e2f8a07a10bd
Nekooo <mahdinekooo@gmail.com>
2025-08-16 16:37:41 +0330

    Working on Main

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@
 import { Routes, Route } from 'react-router-dom'
 
 import Header from './Components/Header/Header'
+import Main from './Components/Main/Main'
 
 const App = () => {
   return (
     <div className='px-[10rem] py-2 max-xl:px-[1rem] relative font-poppins'>
       <Header/>
+      <Main/>
     </div>
   )
 }
